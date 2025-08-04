a380e62a53fae67acf4849267b8a16a2f2278320
cakelake1 <nn.doronin@gmail.com>
2025-08-04 17:55:26 +0300

    11 task

---
+++ 1.3/11_task.py
@@ -0,0 +1,14 @@
+def BigMinus(s1, s2): 
+    check_lengh = len(s1) < len(s2) or (len(s1) == len(s2) and s1 < s2)
+    a, b = ((s1, s2), (s2, s1))[check_lengh]
+    b = b.zfill(len(a))
+    list = []
+    debt = 0
+    for i in range(len(a)-1, -1, -1):
+        digit = int(a[i]) - int(b[i]) - debt
+        debt = digit < 0
+        digit += debt * 10
+        list.append(str(digit))
+    result = ''.join(reversed(list))
+    result = result.lstrip('0')
+    return result
