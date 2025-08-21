59a7c9cc253d7e013a8cd5d902cd76d4236a7b22
cakelake1 <nn.doronin@gmail.com>
2025-08-21 15:47:55 +0300

    18 update

--- 1.3/18_task.py
+++ 1.3/18_task.py
@@ -1,7 +1,16 @@
 def MisterRobot(N, data):
-    invers = 0
+    arr = list(data)
     for i in range(N):
-        for j in range(i+1,N):
-            if data[i] > data[j]:
-                invers +=1
-    return invers % 2 == 0
+        while arr[i] != i + 1:
+            p = i
+            while p < N and arr[p] != i + 1:
+                p += 1
+            if p == N:
+                return False
+            if p - i >= 2:
+                arr[p-2], arr[p-1], arr[p] = arr[p-1], arr[p], arr[p-2]
+            elif i < N - 2:
+                arr[i], arr[i+1], arr[i+2] = arr[i+1], arr[i+2], arr[i]
+            else:
+                return False
+    return True
