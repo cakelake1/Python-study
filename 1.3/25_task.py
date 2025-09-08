d5eaf781ebcd62a672055db9de4a0cade04fca2e
cakelake1 <nn.doronin@gmail.com>
2025-09-08 21:35:35 +0300

    25 task

---
+++ 1.3/25_task.py
@@ -0,0 +1,14 @@
+def TransformTransform(A, N):
+    def S(massivea):
+        B = []
+        n = len(massivea)
+        for i in range(n):
+            for j in range(n - i):
+                k = i + j
+                max_value = max(massivea[j:k+1])
+                B.append(max_value)
+        return B
+    first_transform = S(A)
+    transform_double = S(first_transform)
+    result = sum(transform_double) 
+    return result % 2 == 0
