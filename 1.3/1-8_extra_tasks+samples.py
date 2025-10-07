6af2ba616e7a3b06341fadcf07ae23b2c098d9fe
cakelake1 <nn.doronin@gmail.com>
2025-10-07 12:13:24 +0300

    task 4-6 corrected

--- 1.3/1-8_extra_tasks+samples.py
+++ 1.3/1-8_extra_tasks+samples.py
@@ -35,13 +35,15 @@ def length_check_pop(r_list):
         r_list.pop(0)
         return 1 + length_check_pop(r_list)
 """ print(length_check_pop([1,2,3,4])) """
-def poliandrom_check(r_word):
-    r_word =r_word.lower().replace(' ','')
-    if len(r_word) <= 1:
+def poliandrom_check(r_word, start = 0, end = 0):
+    if end == 0:
+        r_word =r_word.lower().replace(' ','')
+        end = len(r_word) - 1
+    if start >= end:
         return True
-    if r_word[0] != r_word[-1]:
+    if r_word[start] != r_word[end]:
         return False
-    return poliandrom_check(r_word[1:-1])
+    return poliandrom_check(r_word, start + 1, end - 1)
 """ print(poliandrom_check('a'))
 print(poliandrom_check('ab'))
 print(poliandrom_check('anna'))
@@ -57,18 +59,18 @@ print(poliandrom_check('acab')) """
 print_even_numbers(1234)
 print_even_numbers(1203456789101112)
 print_even_numbers([1,2,3,4]) """
-def print_even_numbers_list(r_list2):
-    if not r_list2:
+def print_even_numbers_list(r_list2, start = 0):
+    if start >= len(r_list2):
         return 0
-    if r_list2[0] % 2 == 0:
-        print(r_list2[0], end='')
-    return print_even_numbers_list(r_list2[1:])
-print_even_numbers_list([1,2,3,4])
-""" def print_even_index_list(r_list3):
-    if not r_list3:
+    if r_list2[start] % 2 == 0:
+        print(r_list2[start], end='')
+    return print_even_numbers_list(r_list2, start + 1)
+""" print_even_numbers_list([1,2,3,4]) """
+def print_even_index_list(r_list3, start = 0):
+    if start >= len(r_list3):
         return 0
-    print(r_list3[0], end='')
+    print(r_list3[start], end='')
     if len(r_list3) > 2:
-        return print_even_index_list(r_list3[2:])
+        return print_even_index_list(r_list3, start + 2)
 print_even_index_list([1,2,3,4])
-print_even_index_list([1,2,0,3,4,5,6,7,8,9,1,0,1,1,1,2]) """
+print_even_index_list([1,2,0,3,4,5,6,7,8,9,1,0,1,1,1,2])
--- 1.3/1-8_extra_tasks.py
+++ 1.3/1-8_extra_tasks.py
@@ -2,34 +2,41 @@ def exponentiation(n,m):
     if m == 0:
         return 1
     return n * (exponentiation(n, m-1))
+
 def sum_digit(s):
     if s < 0:
         return -sum_digit(-s)
     if s < 10:
         return s
     return s%10 + sum_digit(s//10)
+
 def length_check_pop(r_list):
     if r_list == []:
         return 0
     r_list.pop(0)
     return 1 + length_check_pop(r_list)
-def poliandrom_check(r_word):
-    r_word =r_word.lower().replace(' ','')
-    if len(r_word) <= 1:
+
+def poliandrom_check(r_word, start = 0, end = 0):
+    if end == 0:
+        r_word =r_word.lower().replace(' ','')
+        end = len(r_word) - 1
+    if start >= end:
         return True
-    if r_word[0] != r_word[-1]:
+    if r_word[start] != r_word[end]:
         return False
-    return poliandrom_check(r_word[1:-1])
-def print_even_numbers_list(r_list2):
-    if not r_list2:
+    return poliandrom_check(r_word, start + 1, end - 1)
+
+def print_even_numbers_list(r_list2, start = 0):
+    if start >= len(r_list2):
         return 0
-    if r_list2[0] % 2 == 0:
-        print(r_list2[0], end='')
-    return print_even_numbers_list(r_list2[1:])
-def print_even_index_list(r_list3):
-    if not r_list3:
+    if r_list2[start] % 2 == 0:
+        print(r_list2[start], end='')
+    return print_even_numbers_list(r_list2, start + 1)
+
+def print_even_index_list(r_list3, start = 0):
+    if start >= len(r_list3):
         return 0
-    print(r_list3[0], end='')
+    print(r_list3[start], end='')
     if len(r_list3) > 2:
-        return print_even_index_list(r_list3[2:])
+        return print_even_index_list(r_list3, start + 2)
 
