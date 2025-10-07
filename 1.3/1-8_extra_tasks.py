dddd316903e8589750cecafbd21f65cf5f62771d
cakelake1 <nn.doronin@gmail.com>
2025-10-07 13:16:47 +0300

    Update 1-8_extra_tasks.py

--- 1.3/1-8_extra_tasks.py
+++ 1.3/1-8_extra_tasks.py
@@ -3,6 +3,7 @@ def exponentiation(n,m):
         return 1
     return n * (exponentiation(n, m-1))
 
+
 def sum_digit(s):
     if s < 0:
         return -sum_digit(-s)
@@ -10,11 +11,13 @@ def sum_digit(s):
         return s
     return s%10 + sum_digit(s//10)
 
+
 def length_check_pop(r_list):
     if r_list == []:
         return 0
     r_list.pop(0)
     return 1 + length_check_pop(r_list)
+
 
 def poliandrom_check(r_word, start = 0, end = 0):
     if end == 0:
@@ -26,12 +29,14 @@ def poliandrom_check(r_word, start = 0, end = 0):
         return False
     return poliandrom_check(r_word, start + 1, end - 1)
 
+
 def print_even_numbers_list(r_list2, start = 0):
     if start >= len(r_list2):
         return 0
     if r_list2[start] % 2 == 0:
         print(r_list2[start], end='')
     return print_even_numbers_list(r_list2, start + 1)
+
 
 def print_even_index_list(r_list3, start = 0):
     if start >= len(r_list3):
