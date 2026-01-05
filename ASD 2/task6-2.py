58d53663a59894021769b6f853c966c3ec4241e1
cakelake1 <nn.doronin@gmail.com>
2026-01-06 00:26:23 +0300

    исправил ошибки доделал задания *

--- ASD 2/task6-2.py
+++ ASD 2/task6-2.py
@@ -3,13 +3,12 @@
 # addHead/removeHead O(n) мы добавляем/удаляем в начале списка из за которого все элементы сдвигаются и сложность увеличивается
 
 # 7.2. Добавьте для каждого из четырёх вышеупомянутых методов тесты: проверяйте изменившуюся длину очереди и наличие или отстутствие в ней добавляемого/удаляемого элемента.
-class Queue:
+class Deque:
     def __init__(self):
         self.queue = [] 
 
     def enqueue(self, item):
         self.queue.append(item)
-        
 
     def dequeue(self):
         if self.size() == 0:
@@ -65,17 +64,134 @@ def is_palindrome(s):
     return True
 # 7.4 *  Напишите метод, который возвращает минимальный элемент деки за O(1).
 # Если мы говорим только о реализации метода, то:
-def __init__(self):
+class Deque_Min:
+    def __init__(self):
         self.deque = []
-        self.min_stack = []
-    
-def get_min(self):
-    if not self.min_stack:
+        self.min_stack = []  
+
+    def addFront(self, item):
+        self.deque.append(item)
+        if not self.min_stack or item <= self.min_stack[-1]:
+            self.min_stack.append(item)
+
+    def addTail(self, item):
+        self.deque.insert(0, item)
+        if not self.min_stack  or item <= self.min_stack[-1]:
+            self.min_stack.append(item)
+
+    def removeFront(self):
+        if self.size() > 0:
+            item = self.deque.pop()
+            if self.min_stack and item == self.min_stack[-1]:
+                self.min_stack.pop()
+            return item
         return None
-    return self.min_stack[-1]
+
+    def removeTail(self):
+        if self.size() > 0:
+            item = self.deque.pop(0)
+            if self.min_stack and item == self.min_stack[-1]:
+                self.min_stack.pop()
+            return item
+        return None
+
+    def size(self):
+        return len(self.deque)
+    
+    def get_min(self):
+        if self.min_stack:
+            return self.min_stack[-1]
+        return None
 
 # 7.5.* Реализуйте двустороннюю очередь с помощью динамического массива. Методы добавления и удаления элементов с обоих концов деки должны работать за амортизированное время o(1).
+class Deque75:
+    def __init__(self):
+        self.stack = []
+        self.start = 0
+        self.count = 0
+    
+    def addFront(self, item):
+        if self.count == len(self.stack):
+            self._increase()
+        if self.start == 0:
+            self.start = len(self.stack) - 1
+        else:
+            self.start -= 1
+        self.stack[self.start] = item
+        self.count += 1
+    
+    def addTail(self, item):
+        if self.count == len(self.stack):
+            self._increase()
+        index = (self.start + self.count) % len(self.stack)
+        self.stack[index] = item
+        self.count += 1
+    
+    def removeFront(self):
+        if self.count == 0:
+            return None
+        item = self.stack[self.start]
+        self.stack[self.start] = None
+        self.start = (self.start + 1) % len(self.stack)
+        self.count -= 1
+        if self.count > 0 and self.count * 4 <= len(self.stack):
+            self._decrease()
+        return item
+    
+    def removeTail(self):
+        if self.count == 0:
+            return None
+        index = (self.start + self.count - 1) % len(self.stack)
+        item = self.stack[index]
+        self.stack[index] = None
+        self.count -= 1
+        if self.count > 0 and self.count * 4 <= len(self.stack):
+            self._decrease()
+        return item
+    
+    def size(self):
+        return self.count
+    
+    def _increase(self):
+        old_size = len(self.stack)
+        new_size = max(1, old_size * 2)
+        new_stack = [None] * new_size
+        for i in range(self.count):
+            old_index = (self.start + i) % old_size
+            new_stack[i] = self.stack[old_index]
+        self.stack = new_stack
+        self.start = 0
+    
+    def _decrease(self):
+        old_size = len(self.stack)
+        new_size = max(4, old_size // 2)
+        new_stack = [None] * new_size
+        for i in range(self.count):
+            old_index = (self.start + i) % old_size
+            new_stack[i] = self.stack[old_index]
+        self.stack = new_stack
+        self.start = 0
 
 # 7.6.* Напишите автономную функцию, которая проверяет правильность расстановки (баланс) скобок в символьном (например, арифметическом) выражении. Оно подаётся на вход функции как строка, например "(())}{(" или "[]({})". Алгоритм должен обрабатывать и учитывать круглые (), квадратные [] и фигурные {} скобки. Строка всегда корректна: содержит только скобки указанных типов. Алгоритм должен работать за O(N), где N -- длина выражения.
 #Внутри этой функции используйте стек.
+def balance_brackets(stroke):
+    stack = []
+    open_chars = '([{'
+    close_chars = ')]}'
+    for char in stroke:
+        if char in open_chars:
+            stack.append(char)
+        elif char in close_chars:
+            if len(stack) == 0:
+                return False
+            close_char_index = close_chars.index(char)
+            match_char = open_chars[close_char_index]
+            if stack[-1] != match_char:
+                return False
+            stack.pop()
+    if len(stack) != 0:
+        return False
+    return True
+        
 # тут я не успеваю немного с двумя задачами, доделаю 23,24 декабря и дальнейшую рефлексию.
+# доделал только 5 января
--- ASD 2/task6-3.py
+++ ASD 2/task6-3.py
@@ -23,3 +23,41 @@ print(is_palindrome("арозаупаланалапуазора"))  # True
 print(is_palindrome("казак"))                   # True
 print(is_palindrome("шалаш"))                   # True
 print(is_palindrome("hello"))
+
+#7.4*
+dq = Deque_Min()
+dq.addFront(5)
+dq.addTail(3)
+dq.addFront(2)
+print(dq.get_min())
+dq.removeFront()
+print(dq.get_min())
+dq.removeTail()
+print(dq.get_min())
+
+#7.5*
+dq = Deque75()
+print(dq.size())
+print(dq.removeFront())
+print(dq.removeTail())
+dq.addTail(10)
+print(dq.size())
+dq.addTail(20)
+print(dq.size())
+dq.addFront(5)
+print(dq.size())
+print(dq.removeTail())
+print(dq.removeFront())
+print(dq.size())
+print(dq.removeTail())
+print(dq.size())
+
+#7.6*
+print(balance_brackets("()"))
+print(balance_brackets("(]"))
+print(balance_brackets(""))
+print(balance_brackets("[]"))
+print(balance_brackets("{}"))
+print(balance_brackets("()[]{}"))
+print(balance_brackets("({[]})"))
+print(balance_brackets("{[()()]}"))
