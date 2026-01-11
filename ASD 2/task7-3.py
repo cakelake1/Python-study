4d28c5cd06ef9a0c290dde8c3fb4cf31bdc5d2fb
cakelake1 <nn.doronin@gmail.com>
2026-01-11 23:33:23 +0300

    добавил тесты к 8,9*

--- ASD 2/task7-3.py
+++ ASD 2/task7-3.py
@@ -1,40 +1,5 @@
-
-list1 = OrderedList(True)
-print([node.value for node in list1.get_all()])
-
-list1.add(3)
-list1.add(1)
-list1.add(2)
-print([node.value for node in list1.get_all()])
-
-node = list1.find(2)
-print(f"{node.value if node else None}")
-
-list1.delete(2)
-print([node.value for node in list1.get_all()])
-
-list2 = OrderedList(False)
-list2.add(3)
-list2.add(1)
-list2.add(2)
-print([node.value for node in list2.get_all()])
-
-list3 = OrderedStringList(True)
-list3.add("  арбуз  ")
-list3.add("баобаб")
-list3.add("скворешник")
-list3.add("ябеда")
-print([node.value for node in list3.get_all()])
-
-list1.clean(False)
-list1.add(3)
-list1.add(1)
-list1.add(2)
-print([node.value for node in list1.get_all()])
-
-print(list1.len())
-
-print("добавление по возрастанию:")
+# main_test Добавьте тесты для добавления, удаления и поиска элемента по его значению -- каждый случай с учётом признака упорядоченности.
+# тест на добавление по возрастанию
 list1 = OrderedList(True)
 list1.add(5)
 list1.add(1)
@@ -43,7 +8,7 @@ list1.add(7)
 list1.add(2)
 print([node.value for node in list1.get_all()])  #  [1, 2, 3, 5, 7]
 
-print("\nдобавление по убыванию:")
+# тест на добавление по убыванию
 list2 = OrderedList(False)
 list2.add(5)
 list2.add(1)
@@ -52,43 +17,94 @@ list2.add(7)
 list2.add(2)
 print([node.value for node in list2.get_all()])  #  [7, 5, 3, 2, 1]
 
-print("\nпоиск по возрастанию:")
+# тест на поиск по возрастанию
 list3 = OrderedList(True)
-list3.add(1); list3.add(3); list3.add(5); list3.add(7); list3.add(9)
-print("Найден 5:", list3.find(5).value if list3.find(5) else None)  #  5
-print("Не найден 4:", list3.find(4))  #  None
-print("Не найден 10:", list3.find(10))  #  None
+list3.add(1)
+list3.add(3)
+list3.add(5)
+list3.add(7)
+list3.add(9)
+print("5", list3.find(5).value if list3.find(5) else None)  #  5
+print("4", list3.find(4))  #  None
+print("10", list3.find(10))  #  None
 
-print("\nПоиск по убыванию:")
+# тест на поиск по убыванию
 list4 = OrderedList(False)
-list4.add(9); list4.add(7); list4.add(5); list4.add(3); list4.add(1)
-print("Найден 5:", list4.find(5).value if list4.find(5) else None)  #  5
-print("Не найден 6:", list4.find(6))  #  None
-print("Не найден 0:", list4.find(0))  #  None
+list4.add(9)
+list4.add(7)
+list4.add(5)
+list4.add(3)
+list4.add(1)
+print("5", list4.find(5).value if list4.find(5) else None)  #  5
+print("6", list4.find(6))  #  None
+print("0", list4.find(0))  #  None
 
-print("\nУдаление по возрастанию:")
+# тест на удаление по возрастанию
 list5 = OrderedList(True)
-list5.add(1); list5.add(2); list5.add(3); list5.add(3); list5.add(4)
-print("До удаления:", [node.value for node in list5.get_all()])
+list5.add(1)
+list5.add(2)
+list5.add(3)
+list5.add(3)
+list5.add(4)
+print([node.value for node in list5.get_all()])
 list5.delete(3)
-print("После удаления первой 3:", [node.value for node in list5.get_all()])  #  [1, 2, 3, 4]
+print([node.value for node in list5.get_all()])  #  [1, 2, 3, 4]
 
-print("\nУдаление по убыванию:")
+# тест на удаление по убыванию
 list6 = OrderedList(False)
-list6.add(4); list6.add(3); list6.add(3); list6.add(2); list6.add(1)
-print("До удаления:", [node.value for node in list6.get_all()])
+list6.add(4)
+list6.add(3)
+list6.add(3)
+list6.add(2)
+list6.add(1)
+print([node.value for node in list6.get_all()])
 list6.delete(3)
-print("После удаления первой 3:", [node.value for node in list6.get_all()])  #  [4, 3, 2, 1]
+print([node.value for node in list6.get_all()])  #  [4, 3, 2, 1]
 
-print("\nУдаление несуществующего элемента:")
+# тест на удаление по граничного значения
 list7 = OrderedList(True)
-list7.add(1); list7.add(2); list7.add(3)
-print("До удаления:", [node.value for node in list7.get_all()])
+list7.add(1)
+list7.add(2)
+list7.add(3)
+print([node.value for node in list7.get_all()])
 list7.delete(5)  # Не существует
-print("После удаления 5:", [node.value for node in list7.get_all()])  #  [1, 2, 3]
+print([node.value for node in list7.get_all()])  #  [1, 2, 3]
 
-print("\nУдаление из пустого списка:")
+# тест на удаление элемента в пустом списке
 list8 = OrderedList(True)
-list8.delete(1)  # Не должно быть ошибки
-print("Пустой список:", [node.value for node in list8.get_all()])  #  []
+list8.delete(1)  
+print([node.value for node in list8.get_all()])  #  []
 
+
+# 8* тест на удаление элемента дубликатов
+ol = OrderedList(True)
+for num in [1, 1, 2, 2, 2, 3, 4, 4, 5]:
+    ol.add(num)
+print([node.value for node in ol.get_all()])
+ol.delete_duplicate()
+print([node.value for node in ol.get_all()])
+print(ol.len())
+
+
+# 9 * Тест на списки по возрастанию
+list1 = OrderedList(True)
+for v in [1, 3, 5]: list1.add(v)
+list2 = OrderedList(True)
+for v in [2, 4, 6]: list2.add(v)
+merged = list1.merge(list2)
+print([n.value for n in merged.get_all()])
+# Тест на списки по убыванию
+list3 = OrderedList(False)
+for v in [5, 3, 1]: list3.add(v)
+list4 = OrderedList(False)
+for v in [6, 4, 2]: list4.add(v)
+merged2 = list3.merge(list4)
+print([n.value for n in merged2.get_all()])
+
+# Тест на списки c одинаковыми значениями
+list7 = OrderedList(True)
+for v in [1, 1, 1]: list7.add(v)
+list8 = OrderedList(True)
+for v in [1, 1]: list8.add(v)
+merged4 = list7.merge(list8)
+print([n.value for n in merged4.get_all()])
