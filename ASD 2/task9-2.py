6c4c02cecd93cc2993c311ec064079af2e87f225
cakelake1 <nn.doronin@gmail.com>
2026-01-19 23:23:58 +0300

    доделал задания со *, добавил тесты

--- ASD 2/task9-2.py
+++ ASD 2/task9-2.py
@@ -4,8 +4,83 @@
 
 #5.* Реализуйте словарь с использованием упорядоченного списка по ключу для оптимизации производительности поиска. Оцените временную сложность операций вставки, удаления и поиска в таком словаре.
 
-# Пока не приходи в голову решение, займусь 13.01
+class NativeDictionary:
+    def __init__(self, sz):
+        self.size = sz
+        self.slots = []
+        self.values = []
+        self.keys = []
+
+    def hash_fun(self, key):
+        slot_index = 0
+        for char in key:
+            slot_index += ord(char)
+        return slot_index % self.size 
+
+    def is_key(self, key):
+        for i in self.keys:
+            if i == key:
+                return True
+        return False
+
+    def put(self, key, value):
+        if key in self.keys:
+            key_index = self.keys.index(key)
+            value_index = self.slots[key_index][1]
+            self.values[value_index] = value
+        else:
+            self.values.append(value)
+            value_index = len(self.values) - 1
+            new_pair = [key, value_index]
+            pos = 0
+            for i,k in enumerate(self.keys):
+                if k > key:
+                    break
+                pos = i + 1
+            self.keys.insert(pos, key)
+            self.slots.insert(pos, new_pair)
+
+    def get(self, key):
+        for i in range(len(self.slots)):
+            slot_index = i
+            if self.slots[slot_index][0] == key:
+                return self.values[self.slots[slot_index][1]]
+        return None
 
 #6.* Создайте словарь, в котором ключи представлены битовыми строками фиксированной длины. Реализуйте методы добавления, удаления и поиска элементов, используя битовые операции для ускорения работы.
-
-# Решение примерно представляю, сделаю также 13.01
+class BitNativeDictionary:
+    def __init__(self, bit_length):
+        bit_length=32
+        self.bit_length = bit_length
+        self.mask = (1 << bit_length) - 1
+        self.keys = []
+        self.values = []
+    
+    def add(self, key, value):
+        for i in range(len(self.keys)):
+            if self.keys[i] == key:
+                self.values[i] = value
+                return
+        self.keys.append(key & self.mask)
+        self.values.append(value)
+    
+    def remove(self, key):
+        for i in range(len(self.keys)):
+            if self.keys[i] == key:
+                remove_value = self.values.pop(i)
+                self.keys.pop(i)
+                return remove_value
+        return None
+    
+    def get(self, key):
+        for i in range(len(self.keys)):
+            if self.keys[i] == key:
+                return self.values[i]
+        return None
+    
+    def find(self, key):
+        for i in self.keys:
+            if i == key:
+                return True
+        return False
+# Решение примерно представляю, сделаю также 13.01. Доделал только 19.01
--- ASD 2/task9-3.py
+++ ASD 2/task9-3.py
@@ -1,36 +1,126 @@
 # Сделайте тесты, проверяющие, как работают put(), is_key() и get():
-# добавление значения по новому ключу
+    # добавление значения по новому ключу
 
 test_1 = NativeDictionary(10)
     
-# Добавление значения по новому ключу
+    # Добавление значения по новому ключу
 test_1.put("name", "Иван")
 print(test_1.get('name'))
 print(test_1.is_key('name'))
 print()
 
-# Добавление значения по уже существующему ключу
+    # Добавление значения по уже существующему ключу
 
 test_1.put("name", "Ваня")
 print(test_1.get('name'))
 print(test_1.is_key('name'))
 print()
 
-# Добавление другого нового ключа
+    # Добавление  нового ключа
 
 test_1.put("age", "21")
 print(test_1.get('age'))
 print()
 
-# Проверка присутствующего ключа
+    # Проверка ключа
 print(test_1.is_key('age'))
 
-# Проверка отсутствующего ключа
+    # Проверка отсутствующего ключа
 print(test_1.is_key('height'))
 
-# Извлечение значения по существующему ключу
+    # Извлечение значения по ключу
 print(test_1.get('age'))
 print(test_1.get('name'))
 
-# Извлечение значения по отсутствующему ключу
+    # Извлечение значения по пустому ключу
 print(test_1.get('height'))
+
+# 5*
+test_1 = NativeDictionary(10)
+    
+    #Добавление значения по новому ключу
+test_1.put("name", "Иван")
+print(test_1.get('name'))
+print(test_1.is_key('name'))
+print()
+
+    # Добавление значения по уже существующему ключу
+test_1.put("name", "Ваня")
+print(test_1.get('name'))
+print(test_1.is_key('name'))
+print()
+
+# Добавление другого нового ключа
+test_1.put("age", "21")
+print(test_1.get('age'))
+print()
+
+# проверка присутствующего ключа
+print(test_1.is_key('age'))
+# проверка отсутствующего ключа
+print(test_1.is_key('height'))
+# извлечение значения по существующему ключу
+print(test_1.get('age'))
+print(test_1.get('name'))
+# извлечение значения по отсутствующему ключу
+print(test_1.get('height'))
+print()
+#дополнительные тесты для проверки сортировки
+test_2 = NativeDictionary(10)
+test_2.put("banana", "желтый")
+test_2.put("apple", "красный")
+test_2.put("cherry", "вишневый")
+print(test_2.get('apple'))
+print(test_2.get('banana'))
+print(test_2.get('cherry'))
+print()
+print(test_2.is_key('apple'))
+print(test_2.is_key('banana'))
+print(test_2.is_key('cherry'))
+print(test_2.is_key('date'))
+
+
+# 6 *
+# Добавление значения по новому ключу
+dict1.add(123, "Иван")
+print(dict1.get(123))
+print(dict1.find(123))
+print()
+
+# Добавление значения по уже существующему ключу
+dict1.add(123, "Ваня")
+print(dict1.get(123))
+print(dict1.find(123))
+print()
+
+# Добавление другого нового ключа
+dict1.add(456, "21")
+print(dict1.get(456))
+print()
+
+# Проверка присутствующего ключа
+print(dict1.find(456))
+
+# Проверка отсутствующего ключа
+print(dict1.find(789))
+
+# Извлечение значения по существующему ключу
+print(dict1.get(456))
+print(dict1.get(123))
+
+# Извлечение значения по отсутствующему ключу
+print(dict1.get(789))
+print()
+
+# Тест удаления
+print("Удаляем ключ 123:")
+print(dict1.remove(123))
+print("После удаления:")
+print(dict1.find(123))
+print(dict1.get(123))
+print()
+
+# Тест удаления несуществующего ключа
+print("Удаляем несуществующий ключ 999:")
+print(dict1.remove(999))
+print()
