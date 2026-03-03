899f154aaddcfb03bebd6417b4e21c4d9f2573b5
cakelake1 <nn.doronin@gmail.com>
2026-03-04 00:42:49 +0300

    Create 12_py_names.py

---
+++ Clean_COde/12_py_names.py
@@ -0,0 +1,12 @@
+my_dictionary = {} - one_hundred_random_pairs_dictionary = {} // словарь на сто случайный пар
+list_dict = [] - one_hundred_values_list = [] // список из 100 значений
+s = os.listdir(os.getcwd()) - current_directory_contents = os.listdir(os.getcwd()) // содержимое текущей директории
+im = Image.open(file) - current_image = Image.open(file) // файл текущего изображения с которым работаем.
+full_speed = 0 - total_distance_moto_traveled = 0 // общее пройденное расстояние на мотоцикле
+captured = [] - total_conquest = [] // общие завоеванные клетки
+today_capture = [] - current_day_conquest = [] // клетки завоеванные в текущиц день
+left_part = [] - left_sorted_list = [], right_part = [] - right_sorted_list = [], max_value = [] - max_mid_value = [] // левая, правая отсортированная часть списка, центральное и олдновременно макисмальное значение
+result = [] - result_synchronizing_tables = [] // результат от синхронизации таблиц
+text_lines = [] - words_item_list = [] // список слов
+summ = 0 - total_toner_used = 0 // общий расход тонера
+result = [] - result_of_big_minus = [] // результат функции "Большой минус"
