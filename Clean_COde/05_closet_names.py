10343595bdf80e3d32782f910098cc5b274d0e17
cakelake1 <nn.doronin@gmail.com>
2026-03-11 19:33:31 +0300

    Create 05_closet_names.py

---
+++ Clean_COde/05_closet_names.py
@@ -0,0 +1,12 @@
+# 1. moto_speed = oksana[i]- moto_speed_km_h = oksana[i]
+# 2. current_time = oksana[i + 1] - current_time_hours = oksana[i + 1]
+# 3. one_hundred_values_list = [] - one_hundred_items_list = [] 
+# 4. total_conquest = [] - total_conquest_cages = []
+# 5. result_synchronizing_tables = [] - result_synchronizing = []
+# 6. left_sorted_list = [] - left_ascending_part = []
+# 7. s_no_space = s.replace(" ", "") - name_without_spaces = s.replace(" ", "") 
+# 8. summ = 0 - total_sum_digits = 0
+# 9. summ_Votes = sum(Votes) - total_votes = sum(Votes) 
+# 10.cur_time = 0  - cur_time_sec = 0 
+# 11. map1 = S1.split() - grid1 = S1.split() (tankrush)
+# 12. between = village[index_1 + 1:index_2].count('=') - count_between = village[index_1 + 1:index_2].count('=')
