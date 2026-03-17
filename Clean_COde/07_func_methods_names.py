037d429eaf09cf99115c600947a60333386389d2
cakelake1 <nn.doronin@gmail.com>
2026-03-17 19:59:16 +0300

    Create 07_func_methods_names.py

---
+++ Clean_COde/07_func_methods_names.py
@@ -0,0 +1,12 @@
+# def hash1(self, str1): - def get_primary_hash_index(self, str1): # пример 1
+# def hash2(self, str1): - def get_secondary_hash_index(self, str1): # пример 2
+# def hash_fun(self, key): - def compute_hash(self, key):  - # пример 3
+# def S(massivea): - def get_all_subarray_maximums(massivea): # пример 4
+# def TransformTransform(A, N): - def is_transform_transform_even(A, N): # пример 5
+# def MatrixTurn(Matrix, M, N, T): - def rotate_matrix_right(Matrix, M, N, T): # пример 6
+# def TreeOfLife(H, W, N, tree): - def simulate_tree_life(H, W, N, tree): # пример 7
+# def SherlockValidString(s): - def is_sherlock_valid_string(s): # пример 8
+# def BiggerGreater(input_s): - def get_next_lexicographically_greater(input_s): # пример 9
+# def ShopOLAP(N, items): - def sort_sales_data(N, items): # пример 10
+# def MisterRobot(N, data): - def can_sort_by_triple_rotations(N, data): # пример 11
+# def MaximumDiscount(N, price): - def calculate_maximum_discount(N, price): # пример 11
