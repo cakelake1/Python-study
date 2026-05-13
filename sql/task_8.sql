b3aaeb3985e58716d7b7f5a8ad9ae93af3cea142
cakelake1 <nn.doronin@gmail.com>
2026-05-13 16:27:08 +0300

    Create task_8.sql

---
+++ sql/task_8.sql
@@ -0,0 +1,18 @@
+--9.4.1. Найдите все пары из разных (уникальных) заказчиков (Customers), для которых не задан регион (поле Region).
+
+SELECT t1.ContactName, t2.ContactName, t1.Region
+FROM Customers t1, Customers t2
+WHERE (t1.Region is Null) AND (t2.Region is Null)
+AND t1.ContactName <> t2.ContactName
+
+--9.4.2. Найдите вложенным запросом список заказов (Orders), в котором у заказчиков (Customers) регион не пуст (поле Region).
+
+SELECT * FROM Orders t1 
+WHERE EXISTS 
+(SELECT * FROM Customers t2 
+WHERE t1.CustomerID = t2.CustomerID AND t2.Region is NOT NULL)
+
+--9.4.3. Немного условный, но показательный пример. Найдите все заказы (таблица Orders), цена за доставку товара которых (Freight) превышает цену любого товара (поле UnitPrice, таблица Products).
+
+SELECT * FROM Orders 
+WHERE Freight > ALL( SELECT UnitPrice FROM Products)
