eb7f3e212776ce7a21b3f951417d79edcf17b267
cakelake1 <nn.doronin@gmail.com>
2026-05-18 16:03:51 +0300

    Create task_11.sql

---
+++ sql/task_11.sql
@@ -0,0 +1,15 @@
+--12.3.1. Добавьте нового пользователя в таблицу Employees.
+
+INSERT INTO Employees(LastName, FirstName,Title,TitleOfCourtesy,BirthDate, HireDate, Address, City, Country)
+VALUES ('Doronin', 'Nickolay','Sales Manager', 'Mr.','19910510', '20120510', 'Georgia 1936', 'Zelenograd','RUS' )
+
+--12.3.2. Свяжите этого нового пользователя с какой-либо территорией с помощью таблицы EmployeeTerritories (многие-ко-многим).
+
+INSERT INTO EmployeeTerritories(EmployeeID, TerritoryID)
+VALUES ('12', '125007' )
+
+--12.3.3. Попробуйте добавить новую запись в таблицу заказов Orders. Возникнут ли какие-либо конфликты?
+
+INSERT INTO Orders(EmployeeID)
+VALUES ('12')
+-- Если я добавляю запись с реальным сотрудником, то кофликтов нет, если добавляю с несуществующим, то возникает конфликт по FK(EmployeeID)
