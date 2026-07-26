561225ee2449c9a41a979c79cdb235c4165ced10
cakelake1 <nn.doronin@gmail.com>
2026-07-26 23:42:56 +0300

    убрал вложенные функции

--- asd 22/task_6.py
+++ asd 22/task_6.py
@@ -17,34 +17,36 @@ class BalancedBST:
             self.Root = None
             return
         s_a = sorted(a)
-        def rec_build(left,right,parent):
-            if left > right:
-                return None
-            mid = (left + right) // 2
-            idx_mid = s_a[mid]
-            node = BSTNode(idx_mid, parent)
-            if parent is None:
-                node.Level = 0
-            else:
-                node.Level = parent.Level + 1
-            node.LeftChild = rec_build(left, mid - 1, node)
-            node.RightChild = rec_build(mid + 1, right, node)
-            return node
-        self.Root = rec_build(0, len(s_a) - 1, None)
+        self.Root = self.rec_build(0, len(s_a) - 1,None, s_a)
+    def rec_build(self,left,right,parent,s_a):
+        if left > right:
+            return None
+        mid = (left + right) // 2
+        idx_mid = s_a[mid]
+        node = BSTNode(idx_mid, parent)
+        if parent is None:
+            node.Level = 0
+        else:
+            node.Level = parent.Level + 1
+        node.LeftChild = self.rec_build(left, mid - 1, node,s_a)
+        node.RightChild = self.rec_build(mid + 1, right, node, s_a)
+        return node
+        
     def IsBalanced(self, root_node):
         if root_node is None:
             return True
-        def rec_balance(node):
-            if node  is None:
-                return 0
-            left_side = rec_balance(node.LeftChild)
-            if left_side == -1:
-                return -1
-            right_side = rec_balance(node.RightChild)
-            if right_side == -1:
-                return -1
-            if abs(left_side - right_side) > 1:
-                return -1
-            result = 1 + max(left_side, right_side)
-            return result
-        return rec_balance(root_node) != -1
+        return self.rec_balance(root_node) != -1
+    def rec_balance(self,node):
+        if node  is None:
+            return 0
+        left_side = self.rec_balance(node.LeftChild)
+        if left_side == -1:
+            return -1
+        right_side = self.rec_balance(node.RightChild)
+        if right_side == -1:
+            return -1
+        if abs(left_side - right_side) > 1:
+            return -1
+        result = 1 + max(left_side, right_side)
+        return result
+        
